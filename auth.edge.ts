dd97beb7f53a6c5a570581e02d4305080fb37a6e
fahasfapyf-droid <fahasfa.pyf@gmail.com>
2026-07-24 10:39:37 +0500

    Separate Edge and Node auth configuration

--- auth.edge.ts
+++ auth.edge.ts
@@ -1,4 +1,34 @@
 import NextAuth from "next-auth";
-import authConfig from "./auth.config";
 
-export const { auth: middleware } = NextAuth(authConfig);
+export const { auth: middleware } = NextAuth({
+  pages: {
+    signIn: "/login",
+  },
+
+  session: {
+    strategy: "jwt",
+  },
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+      }
+
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+        session.user.role = token.role as string;
+      }
+
+      return session;
+    },
+  },
+
+  // IMPORTANT
+  providers: [],
+});
